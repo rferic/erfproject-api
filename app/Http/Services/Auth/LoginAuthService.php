bb5d66dac60a67f621d1922af1875bd0ba732f09
Eric Rojas Fuerte <erojas@labelgrup.com>
2020-09-13 12:16:09 +0200

    Return me in login

--- app/Http/Services/Auth/LoginAuthService.php
+++ app/Http/Services/Auth/LoginAuthService.php
@@ -4,6 +4,7 @@
 namespace App\Http\Services\Auth;
 
 
+use App\Http\Resources\User\UserResource;
 use Carbon\Carbon;
 
 class LoginAuthService
@@ -36,6 +37,7 @@ class LoginAuthService
         $token->save();
 
         return [
+            'me' => New UserResource(auth()->user()),
             'access_token' => $tokenResult->accessToken,
             'expires_at' => Carbon::parse($token->expires_at)->toDateTimeString()
         ];
